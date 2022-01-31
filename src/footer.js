50a145fc47c60fb605d9cfa07e5f2a2bb0236022
Rhythm-Adhikari <57981837+Rhythm-Adhikari@users.noreply.github.com>
2022-01-31 22:18:58 +1100

    Add files via upload
    footer

--- src/footer.js
+++ src/footer.js
@@ -1,29 +1,45 @@
-import './footer.css';
-import React from 'react'
-import logo from './logo.svg';
-function foot() {
+import React from "react";
+import { render } from "react-dom"; 
+
+const footerStyle = {
+  backgroundColor: "#E84857",
+  fontSize: "20px",
+  color: "white",
+  borderTop: "1px solid",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+const phantomStyle = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "90%"
+};
+
+function Footer({ children }) {
   return (
-
-<div id="foot">
-  <ul id="dub">
-    <li id="micro">
-      <img src={logo} alt="logo" />
-      <a href="#home"style={{color: "Black", textDecoration: "none", fontWeight: "bold", fontSize: 30}}>Resturants</a> &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-      <a href="#news"style={{color: "Black", textDecoration: "none", fontWeight: "bold", fontSize: 30}}>Become partner</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-      <a href="#contact"style={{color: "Black", textDecoration: "none", fontWeight: "bold", fontSize: 30}}>Contact</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-      <a href="#about"style={{color: "Black", textDecoration: "none", fontWeight: "bold", fontSize: 30, backgroundColor: "#E84857", borderRadius: 10}}>Login</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-      <a href="#about"style={{color: "Black", textDecoration: "none", fontWeight: "bold", fontSize: 30}}>AboutUs</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-    </li>
-  </ul>
-
-</div>
-
-  
-    
-
-
-
+    <div>
+      <div style={phantomStyle} />
+      <div style={footerStyle}>{children}</div>
+    </div>
   );
 }
+render(
+  <div>	
+    <Footer>
+      <ul>
+	<li>lalalal</li>
+	<li>lalalal</li>
+      </ul>
+    </Footer>
+  </div>,
+  document.getElementById("roots")
+);
 
-export default foot;
+export default Footer;
