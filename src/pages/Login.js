1dcf00e0a111893cf22f8d5881a5dd1e0344e137
Rhythm-Adhikari <57981837+Rhythm-Adhikari@users.noreply.github.com>
2022-03-16 20:37:10 +1100

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ const login = () => {
             <h1 >log in to continue</h1>
             <input type='text' className='username' placeholder='username'/>
             <input type='password' className='password' placeholder='password'/>
-            <button className='button'>Log in > </button>
+            <button className='button'>Log in ></button>
             <text className='register'>No account yet? <Link to="/Register" style={{ color:'white' }}>Register here</Link></text>
             <text className='trouble'>Trouble signing in? <Link to="/trouble_sigin_in" style={{ color:'white' }}>Click here</Link></text>
             
