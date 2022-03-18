9408b58c7dbd6f9c842e27e422b7c35a7c7d3d17
junshengli <junshengli2020@gmail.com>
2022-03-18 13:11:15 +1100

    small addon homepage

--- src/pages/HomeImage.js
+++ src/pages/HomeImage.js
@@ -8,6 +8,22 @@ const HomeImage = () =>{
             <text className="big_description">Best <font color="EC4C5A">Food</font> Delivery in your area</text>
             <text className="small_description"> For A Quality Dining Experience </text>
             <button className="partner_button">Become our partner</button>
+
+            <div className="sec">
+                <text className="sec_title">Dining <font color="EC4C5A">opporrtunities</font> near you</text>
+                <div className="rests">
+                    <div className="rest">
+                        
+                    </div>
+                    <div className="rest">
+                    </div>
+                    <div className="rest">
+                    </div>
+                </div>
+               
+            </div>
+
+          
         </div>
     )
 }
