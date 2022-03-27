6d73b8bbea924062a14cbc679f11c2939addfe3e
Rhythm-Adhikari <57981837+Rhythm-Adhikari@users.noreply.github.com>
2022-03-27 16:28:41 +1100

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,7 @@ function Login (){
     console.warn(email,password)
     let item={email,password};
     
-    let result= await fetch("http://betterdinnerapi.herokuapp.com/api/login",{
+    let result= await fetch("https://api.betterdinner.space/api/login",{
         method:'POST',
         headers:{
             "Content-Type":"application/json",
