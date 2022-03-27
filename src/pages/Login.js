2effff8d7e7f3796b13761417703621094f88272
DUMBASS <rhythm12_adhikari@hotmail.com>
2022-03-28 10:54:30 +1100

    localstorage fix-- only works 10%of the time

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from 'react';
 import bgimg from './login.png';
 import { Link } from 'react-router-dom';
 import './Login.css';
+import LoginHeader from "../components/loginHeader";
 
 function Login (){
+
 
   //login api integration
   const [email,setEmail]=useState("")
@@ -14,22 +16,23 @@ function Login (){
 
   async function login()
   {
-    console.warn(email,password)
-    let item={email,password};
-    
 
-    let result= await fetch("https://cors-anywhere.herokuapp.com/https://api.betterdinner.space/api/login",{
-         method:'POST',
-         headers:{
-            "Content-Type":"application/json",
-            "Accept":'application/json',
-           }, 
-        body:JSON.stringify(item)
-      });
-      
+      let item={email,password}
+      console.warn(item)
+        let result= await fetch("https://cors-anywhere.herokuapp.com/https://api.betterdinner.space/api/login",{
+            method: 'POST',
+            headers: {
+                "Content-Type":"application/json",
+                "Accept":'application/json',
+               },
+            body:JSON.stringify(item)
+
+        });
+
 
       result = await result.json();
-      sessionStorage.getItem("user-info",JSON.stringify(result))
+      console.warn("result",result)
+      localStorage.setItem("user-info",JSON.stringify(result))
       //history.push("/add")
         
 
@@ -37,8 +40,10 @@ function Login (){
 
 
   return (
-    <div className='main'>  
+
+    <div className='main'>
         <form className='login-form'>
+            <LoginHeader />
             <h1 >log in to continue</h1>
             <input type='text' className='username' onChange={(e)=>setEmail(e.target.value)} placeholder='Email'/>
             <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} className='password' placeholder='Password'/>
