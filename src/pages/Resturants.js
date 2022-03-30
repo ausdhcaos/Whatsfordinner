14fd95f9e569f7f00e005a0c1022b1fe51e22b4a
nguyenminh2440 <nguyenminh2440@gmail.com>
2022-03-30 12:01:55 +0700

    Restaurants page need testing

--- src/pages/Resturants.js
+++ src/pages/Resturants.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import background from "./home.jpg";
+import './Restaurants.css';
 
 const resturants = () => {
   return (
-    <div>resturants</div>
+    <div className="restaurants">
+      <img className="image" src={background} alt="homeImg"/>
+      <div className="buttons">
+                <text className="buttons_title">Dining <font color="EC4C5A">opportunities</font> near you</text>
+                <div className="choices">
+                    <div className="choice">      
+                    </div>
+                    <div className="choice">
+                    </div>
+                    <div className="choice">
+                    </div>
+                    <div className="choice">
+                    </div>
+                </div>         
+      </div>
+    </div>
+
   )
 }
 
