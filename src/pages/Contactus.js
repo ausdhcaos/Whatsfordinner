1892cc08bededf3bb1b51fc3bab46a0499138d53
rhythm <rhythm12_adhikari@hotmail.com>
2022-04-21 21:57:45 +1000

    input validation for phone number

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 
 
 function Contactus(){
+
     const [agree, setAgree] = useState(false);
     const chkbox = () => {
         setAgree(!agree);
@@ -15,7 +16,15 @@ function Contactus(){
 
                 <input type='text' className='fullname' placeholder='Full Name'/>
                 <input type='text' className='email' placeholder='Email Address'/>
-                <input type='phone' className='phone' placeholder='Phone number'/>
+                <input
+                    type='phone'
+                    onKeyPress={(event) => {
+                        if (!/[0-9]/.test(event.key)) {
+                            event.preventDefault();
+                        }
+                    }}
+                    className='phone' placeholder='Phone number'/>
+
                 <input type='text' className='text' placeholder='Please enter your message here'/>
                 <input type='checkbox' id='agree' onChange={chkbox} className='contact-lable'  />
                 <h4 >I agree to receive email and other promotional material </h4>
